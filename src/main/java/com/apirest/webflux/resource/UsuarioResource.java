5bf5be059b079bb882a1f7ff6d495a15090b2dec
Leonardo <leeosena21@gmail.com>
2021-08-18 00:19:30 -0300

    Teste de requisição não bloqueante

--- src/main/java/com/apirest/webflux/resource/UsuarioResource.java
+++ src/main/java/com/apirest/webflux/resource/UsuarioResource.java
@@ -1,6 +1,9 @@
 package com.apirest.webflux.resource;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,7 @@ import com.apirest.webflux.service.UsuarioService;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 @RestController
 public class UsuarioResource {
@@ -32,5 +36,13 @@ public class UsuarioResource {
 	@PostMapping(value = "/usuario/insert")
 	public Mono<Usuario> insert(@RequestBody Usuario usuario){
 		return service.save(usuario);
+	}
+	
+	@GetMapping(value = "/usuarios/event", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Tuple2<Long, Usuario>> getUsuarioByEvent(){
+		Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+		Flux<Usuario> events = service.findAll();
+		System.out.println("evento");
+		return Flux.zip(interval, events);
 	}
 }
